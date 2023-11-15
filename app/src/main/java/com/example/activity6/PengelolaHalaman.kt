86832ce40ae99f7c9eabc49b2ad285491a8f41ce
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-11-15 19:00:17 +0700

    First commit, minggu sebelumnya tidak massuk

---
+++ app/src/main/java/com/example/activity6/PengelolaHalaman.kt
@@ -0,0 +1,61 @@
+package com.example.activity6
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.activity6.ui.layout.ContactViewModel
+import com.example.activity6.ui.layout.HalamanDua
+import com.example.activity6.ui.layout.HalamanSatu
+
+enum class PengelolaHalaman {
+    Formulir,
+    Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContactApp(
+    viewModel: ContactViewModel = viewModel(),
+    navHostController: NavHostController = rememberNavController()
+) {
+    Scaffold { innerpadding ->
+        val uiState by viewModel.stateUI.collectAsState()
+
+        NavHost(
+            navController = navHostController,
+            startDestination = PengelolaHalaman.Formulir.name,
+            modifier = Modifier.padding(innerpadding)
+        )
+        {
+            composable(route = PengelolaHalaman.Formulir.name) {
+                HalamanSatu(
+                    onSubmitButtonClicked = {
+                        viewModel.setContact(it)
+                        navHostController.navigate(PengelolaHalaman.Detail.name)
+                    }
+                )
+            }
+
+            composable(route = PengelolaHalaman.Detail.name) {
+                HalamanDua(
+                    contactUiState = uiState,
+                    onBackButtonClicked = {
+                        navHostController.popBackStack(
+                            PengelolaHalaman.Formulir.name,
+                            false
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/activity6/data/ContactUiState.kt
@@ -0,0 +1,7 @@
+package com.example.activity6.data
+
+data class ContactUiState(
+    var nama:String="",
+    var alamat:String="",
+    var tlp:String=""
+)
