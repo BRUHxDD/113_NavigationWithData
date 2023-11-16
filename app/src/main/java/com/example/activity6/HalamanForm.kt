c5ced34fe854cd5dcd2c63e63c2678809bbd73c1
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-11-16 17:26:14 +0700

    Fix on HalamanForm

--- app/src/main/java/com/example/activity6/HalamanForm.kt
+++ app/src/main/java/com/example/activity6/HalamanForm.kt
@@ -40,6 +40,9 @@ fun HalamanForm(
     var phoneText by remember {
         mutableStateOf("")
     }
+
+    var listData: MutableList<String> = mutableListOf(namaText, alamatText, phoneText)
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -81,12 +84,12 @@ fun HalamanForm(
         Row (modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
         ){
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.submit_button))
+            Button(onClick = {onBackButtonCLicked}) {
+                Text(text = stringResource(id = R.string.back_button))
             }
 
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.back_button))
+            Button(onClick = {onSubmitButtonClicked(listData)}) {
+                Text(text = stringResource(id = R.string.submit_button))
             }
         }
     }
