a552b89926e37d1c76d3074e64eff4b547614194
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-11-16 15:58:07 +0700

    Add dimen

--- app/src/main/java/com/example/activity6/MainActivity.kt
+++ app/src/main/java/com/example/activity6/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.activity6.ui.layout.HalamanSatu
 import com.example.activity6.ui.theme.Activity6Theme
 
 class MainActivity : ComponentActivity() {
@@ -41,5 +40,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HalamanSatu {}
+    HalamanHome {}
 }
