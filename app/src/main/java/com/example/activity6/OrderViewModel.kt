4478b606b971be2a9806218003bfedc94792bf47
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-11-16 17:05:49 +0700

    Create OrderViewModel Class file

---
+++ app/src/main/java/com/example/activity6/OrderViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.activity6
+
+import androidx.lifecycle.ViewModel
+import com.example.activity6.data.FormState
+import com.example.activity6.data.OrderUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.text.NumberFormat
+
+private const val HARGA_PER_CUP = 3000
+
+class OrderViewModel : ViewModel() {
+    private val _stateUI = MutableStateFlow((OrderUIState()))
+    private val _nameSTATE = MutableStateFlow(FormState())
+    val stateUI: StateFlow<OrderUIState> = _stateUI.asStateFlow()
+    val nameST : StateFlow<FormState> = _nameSTATE.asStateFlow()
+
+    fun setNama(list: MutableList<String>){
+        _nameSTATE.update { stateSaatIni -> stateSaatIni.copy(
+            nama = list[0],
+            alamat = list[1],
+            phone = list[2]
+        ) }
+    }
+
+    fun setJumlah(jmlEsJumbo: Int) {
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                jumlah = jmlEsJumbo,
+                harga = hitungHarga(jumlah = jmlEsJumbo)
+            )
+        }
+
+    }
+
+    fun setRasa(rasaPilihan: String) {
+        _stateUI.update { stateSaatIni -> stateSaatIni.copy(rasa = rasaPilihan) }
+
+    }
+
+    fun resetOrder() {
+        _stateUI.value = OrderUIState()
+    }
+
+    private fun hitungHarga(jumlah: Int = _stateUI.value.jumlah): String {
+        val kalkulasiHarga = jumlah * HARGA_PER_CUP
+
+        return NumberFormat.getNumberInstance().format(kalkulasiHarga)
+    }
+}
