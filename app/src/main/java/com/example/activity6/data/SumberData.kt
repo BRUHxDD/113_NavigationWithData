c8f8270b8b2ed77c089fc5e6d740ce1bbf1918b7
Teuku Muhammad Attar Fadhilah <attarku05@gmail.com>
2023-11-16 16:48:49 +0700

    Create SumberData Object

---
+++ app/src/main/java/com/example/activity6/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.activity6.data
+
+import com.example.activity6.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.lemon,
+        R.string.lychee,
+        R.string.milk,
+        R.string.vanilla,
+        )
+}
